8c94e946d470737cc18bb5266db77f5eb0ad1f62
NoahBPeterson <noahbpeterson1997@gmail.com>
2020-12-14 14:37:03 -0600

    First commit

---
+++ HelloWorldBranch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldBranch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
