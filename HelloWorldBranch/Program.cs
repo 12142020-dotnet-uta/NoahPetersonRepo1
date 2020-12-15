60502020c93e50965c5175be419ce47cac6a1fdd
NoahBPeterson <noahbpeterson1997@gmail.com>
2020-12-15 14:36:01 -0600

    Returning input

--- HelloWorldBranch/Program.cs
+++ HelloWorldBranch/Program.cs
@@ -7,6 +7,9 @@ namespace HelloWorldBranch
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a word.");
+            string response = Console.ReadLine();
+            Console.WriteLine($"Your response is {response}.");
         }
     }
 }
